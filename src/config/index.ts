b7674d241ff2903bc94fd5e0f4e5cc7f34ab3f4b
CryptoMountains <40394451+CryptoMountains@users.noreply.github.com>
2018-08-02 19:25:35 +0200

    CMT-Blockchain

--- src/config/index.ts
+++ src/config/index.ts
@@ -11,7 +11,7 @@ export default {
       nethash: '20700d78709abd9f48c2c1970eb3202b1076b6dc83b0ed851a9f52dd84c9d1ed',
       token: 'CMT',
       symbol: 'CMT',
-      version: 0x1C, // Addresses will begin with 'A'
+      version: 0x1c, // Addresses will begin with 'A'
       explorer: 'http://178.128.66.43:4200',
       wif: 0xBB, // Network prefix for wif generation
       activePeer: {
@@ -29,7 +29,7 @@ name: 'devnet',
     nethash: '578e820911f24e039733b45e4882b73e301f813a0d2c31330dafda84534ffa23',
     token: 'TEST',
     symbol: 'TEST',
-    version: 0x52, // Addresses will begin with 'a'
+    version: 28, // Addresses will begin with 'a'
     explorer: 'https://dexplorer.ark.io',
     wif: 0xba, // Network prefix for wif generation
     activePeer: {
